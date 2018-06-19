3eb4a2f32f2a2e8034eae29ca102a84f749b4839
Jay.Liao <j99590314@gmail.com>
2018-06-20 00:22:52 +0800

    update run.py

--- run.py
+++ run.py
@@ -6,6 +6,11 @@ from newjob_app.utils import yourator
 from newjob_app import setting
 from threading import Thread
 
+MINUTE = 60
+HOUR = 60 * MINUTE
+# EIGHT_HOURS = 8 * HOUR
+EIGHT_HOURS = 5
+
 
 def run_web_script():
     # start the gunicorn server with custom configuration
@@ -14,19 +19,20 @@ def run_web_script():
 
 
 def scraper():
-    job_files = job_util.get_job_files()
-    # remove out of date job files
-    if len(job_files) > 1:
-        job_files = [int(job_file) for job_file in job_files]
-        job_files = sorted(job_files, reverse=True)
-        for index in range(1, len(job_files)):
-            file_path = os.path.join(setting.JOB_DIRECTORY, str(job_files[index]))
-            file_util.remove_file(file_path=file_path)
-            print(f"remove old job files: {file_path}")
-    print("start download jobs")
-    time.sleep(5)
-    file_name = yourator.download_jobs()
-    print(f"file_name: {file_name}")
+    while True:
+        job_files = job_util.get_job_files()
+        # remove out of date job files
+        if len(job_files) > 1:
+            job_files = [int(job_file) for job_file in job_files]
+            job_files = sorted(job_files, reverse=True)
+            for index in range(1, len(job_files)):
+                file_path = os.path.join(setting.JOB_DIRECTORY, str(job_files[index]))
+                file_util.remove_file(file_path=file_path)
+                print(f"remove old job files: {file_path}")
+        print("start download jobs")
+        time.sleep(EIGHT_HOURS)
+        file_name = yourator.download_jobs()
+        print(f"file_name: {file_name}")
 
 
 def start_scheduler():
